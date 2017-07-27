cc0ec169e2148cc075188e2525b7e590e7b59136
Benny Degezelle <hi@monkeypatch.be>
2017-07-27 18:38:05 +0200

    Set up gem info

--- lib/radiant-if_date_tags-extension.rb
+++ lib/radiant-if_date_tags-extension.rb
@@ -1,8 +1,8 @@
 module RadiantIfDateTagsExtension
   VERSION     = "1.0.0"
   SUMMARY     = "If Date Tags for Radiant CMS"
-  DESCRIPTION = "Makes Radiant better by adding if_date_tags!"
-  URL         = "http://example.com/if_date_tags"
-  AUTHORS     = ["Your Name"]
-  EMAIL       = ["your email"]
+  DESCRIPTION = "Makes Radiant better by adding if_date radius tags"
+  URL         = "http://github.com/jomz/radiant-if_date_tags-extension"
+  AUTHORS     = ["Benny Degezelle"]
+  EMAIL       = ["hi@monkeypatch.be"]
 end
