d8e6b744c7b195a277172d13697f7414699b32f8
Benny Degezelle <hi@monkeypatch.be>
2017-07-28 02:09:57 +0200

    Bump version

--- lib/radiant-if_date_tags-extension.rb
+++ lib/radiant-if_date_tags-extension.rb
@@ -1,5 +1,5 @@
 module RadiantIfDateTagsExtension
-  VERSION     = "1.0.1"
+  VERSION     = "1.0.2"
   SUMMARY     = "If Date Tags for Radiant CMS"
   DESCRIPTION = "Makes Radiant better by adding if_date radius tags"
   URL         = "http://github.com/jomz/radiant-if_date_tags-extension"
